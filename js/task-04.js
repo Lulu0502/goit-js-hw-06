094f2c0da31e7a9aecfd9f39c9ec521b76450664
Lulu0502 <suliya85@gmail.com>
2022-09-15 18:52:12 +0300

    Update task-04.js

---
+++ js/task-04.js
@@ -0,0 +1,18 @@
+"use strict";
+
+const refs = {
+    valueId: document.querySelector("#value"),
+    counterValue: 0,
+    btnDecrement: document.querySelector('[data-action="decrement"]'),
+    btnIncrement: document.querySelector('[data-action="increment"]'),
+}
+
+refs.btnDecrement.addEventListener('click', () => {
+    refs.counterValue -= 1;
+    refs.valueId.textContent = `${refs.counterValue}`;
+});
+
+refs.btnIncrement.addEventListener('click', () => {
+    refs.counterValue += 1;
+    refs.valueId.textContent = `${refs.counterValue}`;
+});
