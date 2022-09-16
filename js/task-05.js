dd5f15d4a61d769e4821684475aff4153301650b
Lulu0502 <suliya85@gmail.com>
2022-09-16 18:36:37 +0300

    task-05

---
+++ js/task-05.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const textInput = document.querySelector('#name-input')
+const output = document.querySelector('#name-output')
+
+textInput.addEventListener('input', event => {
+	output.textContent = event.target.value
+
+	if (event.target.value === '') {
+		output.textContent = 'Anonymous'
+	}
+})
